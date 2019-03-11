968b774a766feb393e3ecc30ce50972ed14e01c4
Boyu Li <boyuli@wirelessprv-10-192-235-234.near.illinois.edu>
2019-03-11 15:37:46 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c86f7ec2086226f5574529cc67f0b37d.js"
+  "/CS498-MP2/precache-manifest.6717bbdadca2ea055c8937f7fa6e45a5.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/CS498-MP2/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
